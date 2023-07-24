da5d879813849ff46d88539400b6888fc9d52e6a
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-07-25 00:31:49 +0530

    Create Max rectangle .cpp

---
+++ Stacks/Max rectangle .cpp
@@ -0,0 +1,75 @@
+class Solution{
+  public:
+    vector<int> nextSmaller(int *heights,int n){
+        vector<int>ans(n);
+        stack<int>stack1;
+        stack1.push(-1);
+        for(int i=n-1;i>=0;i--){
+            int curr = heights[i];
+        while(stack1.top() != -1 && heights[stack1.top()] >= curr){
+            stack1.pop();
+        }
+        ans[i] = stack1.top();
+        stack1.push(i);     
+    }
+        return ans;
+    }
+
+    vector<int> prevSmaller(int *heights,int n){
+        vector<int>ans(n);
+        stack<int>stack1;
+        stack1.push(-1);
+        for(int i=0;i < n;i++){
+            int curr = heights[i];
+        while(stack1.top() != -1 && heights[stack1.top()] >= curr){
+            stack1.pop();
+        }
+        ans[i] = stack1.top();
+        stack1.push(i);     
+    }
+        return ans;
+    }
+
+    int largestRectangleArea(int *heights , int n) {
+
+
+        vector<int>next(n);
+        next = nextSmaller(heights,n);
+
+        vector<int>prev(n);
+        prev = prevSmaller(heights,n);
+        int area = INT_MIN;
+        for(int i=0;i<n;i++){
+            int l = heights[i];
+
+            if(next[i] == -1)
+                next[i] = n;
+
+            int b = next[i] - prev[i] - 1;
+            int Newarea = l*b;
+            area = max(area , Newarea);
+        }
+        return area;
+    }
+  
+    
+    int maxArea(int M[MAX][MAX], int n, int m) {
+        
+        int area = largestRectangleArea(M[0],m);
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(M[i][j] != 0)
+                    M[i][j] = M[i][j]+M[i-1][j];
+                else
+                    M[i][j] = 0;
+                
+            }
+            area = max(area,largestRectangleArea(M[i],m));
+        }
+            
+        return area;
+
+    }
+};
