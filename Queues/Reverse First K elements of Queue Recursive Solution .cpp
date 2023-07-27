4214602feea6cdca497dc864dec9d55127784411
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-07-27 21:36:46 +0530

    Update Reverse First K elements of Queue Recursive Solution .cpp

--- Queues/Reverse First K elements of Queue Recursive Solution .cpp
+++ Queues/Reverse First K elements of Queue Recursive Solution .cpp
@@ -1,4 +1,30 @@
+APPROACH 1 
+TIME COMPLEXIRTY O(N) AND SPACE COMPLEXITY O(K)
+// Function to reverse first k elements of a queue.
+queue<int> modifyQueue(queue<int> q, int k) {  
+    stack<int>ans;
+    for(int i =0 ; i<k ; i++){
+        ans.push(q.front());
+        q.pop();
+    }
+    while( !ans.empty() ){
+        q.push(ans.top());
+        ans.pop();
+    }
+   int n = q.size();
+   for(int i=0 ; i < n - k  ; i++){
+        q.push(q.front());
+        q.pop();
+    }  
+    return q;               
+}
 
+
+
+
+APPROACH 2
+RECURSIVE SOLUTION
+WITH TIME COMPLEXIRTY O(N) AND SPACE COMPLEXITY O(N)
 void ReverseK(queue<int>&q, int k){
     
     if(q.size() == 0 || q.size() == 1)
@@ -15,7 +41,6 @@ void ReverseK(queue<int>&q, int k){
 
 // Function to reverse first k elements of a queue.
 queue<int> modifyQueue(queue<int> q, int k) {
-    
     queue<int>ans;
     for(int i =0 ; i<k && q.size() > 0 ; i++){
         ans.push(q.front());
@@ -25,8 +50,6 @@ queue<int> modifyQueue(queue<int> q, int k) {
     while(q.size() != 0){
         ans.push(q.front());
         q.pop();
-    }
-    
-    return ans;
-               
+    }  
+    return ans;              
 }
