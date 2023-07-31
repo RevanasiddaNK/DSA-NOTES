1b2f610776e6ea776609a2b23e627bec0e15f1e6
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-07-31 22:40:42 +0530

    Update 8.  Diameter of a Binary Tree .cpp

--- BINARY TREES/8.  Diameter of a Binary Tree  .cpp
+++ BINARY TREES/8.  Diameter of a Binary Tree  .cpp
@@ -1,4 +1,5 @@
-/*
+/* 
+TWO APPROACHES
 APPROACH 1   
     TIME COMPLEXITY = O(N^2)
     SPACE COMPLEXITY = O(N^2)
@@ -44,4 +45,53 @@ APPROACH 2 BY NOT MAKING SEPEARATE FUNCTION FOR TO FIND HEIGHT OF TREE
         TIME COPLEXITY    O(N)
         SPACE COMPLEXITY  O(N)
 */
+/* Tree node structure  used in the program
 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+
+private:
+    pair<int, int> FastDiameter(Node* root) {
+        
+        if(root == NULL){
+          pair<int, int> p = make_pair(0,0);
+          return p;
+        }
+        
+        pair<int, int> left1 = FastDiameter(root->left);
+        pair<int, int> right1 = FastDiameter(root->right);
+        
+        
+        
+        int opt1 = left1.first;
+        int opt2 = right1.first;
+        
+        int opt3 = left1.second + right1.second +1;
+    
+        pair<int, int> ans;
+        ans.first= max(opt1, max(opt2,opt3));
+    //    ans.second = opt3;
+        ans.second = max(left1.second , right1.second)+1;
+    
+        return ans;
+        
+    }
+
+public:
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+       
+       return FastDiameter(root).first;
+    }
+};
