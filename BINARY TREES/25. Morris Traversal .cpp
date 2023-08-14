d2705f54833156b12d3fa03d2cb515d0f0769846
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-08-14 19:14:29 +0530

    Update 25. Morris Traversal .cpp

--- BINARY TREES/25. Morris Traversal .cpp
+++ BINARY TREES/25. Morris Traversal .cpp
@@ -1,8 +1,8 @@
-// Inorder Tree Traversal without recursion and without stack & Queue
+// Inorder Tree Traversal without recursion and without stack & Queue LNR
 // Time complexity O(N)
 // Space Complexity O(1)
 
-void MorrisTraversal(struct tNode* root)
+void MorrisTraversalInOrder(struct tNode* root)
 {
     struct tNode *current, *pre;
  
@@ -38,7 +38,92 @@ void MorrisTraversal(struct tNode* root)
                 pre->right = NULL;
                 cout << current->data << " ";
                 current = current->right;
-            } /* End of if condition pre->right == NULL */
-        } /* End of if condition current->left == NULL*/
-    } /* End of while */
+            } 
+        } 
+    } 
 }
+
+// Preorder traversal without recursion and without stack NLR
+
+void morrisTraversalPreorder(node* root)
+{
+    
+    while (root)
+    {
+        // If left child is null, print the current node data. Move to
+        // right child.
+        if (root->left == NULL)
+        {
+            cout<<root->data<<" ";
+            root = root->right;
+        }
+        else
+        {
+            // Find inorder predecessor
+            node* current = root->left;
+            while (current->right && current->right != root)
+                current = current->right;
+ 
+            // If the right child of inorder predecessor already points to
+            // this node
+            if (current->right == root)
+            {
+                current->right = NULL;
+                root = root->right;
+            }
+ 
+            // If right child doesn't point to this node, then print this
+            // node and make right child point to this node
+            else
+            {
+                cout<<root->data<<" ";
+                current->right = root;
+                root = root->left;
+            }
+        }
+    }
+}
+
+// // Morris Traversal for Postorder LRN
+vector<int> postorderTraversal(TreeNode* root)
+{
+    vector<int> res;
+    TreeNode* current = root;
+ 
+    while (current != NULL) {
+        // If right child is null,
+        // put the current node data
+        // in res. Move to left child.
+        if (current->right == NULL) {
+            res.push_back(current->key);
+            current = current->left;
+        }
+        else {
+            TreeNode* predecessor = current->right;
+            while (predecessor->left != NULL
+                   && predecessor->left != current) {
+                predecessor = predecessor->left;
+            }
+            // If left child doesn't point
+            // to this node, then put in res
+            // this node and make left
+            // child point to this node
+            if (predecessor->left == NULL) {
+                res.push_back(current->key);
+                predecessor->left = current;
+                current = current->right;
+            }
+            // If the left child of inorder predecessor
+            // already points to this node
+            else {
+                predecessor->left = NULL;
+                current = current->left;
+            }
+        }
+    }
+    // reverse the res
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+
