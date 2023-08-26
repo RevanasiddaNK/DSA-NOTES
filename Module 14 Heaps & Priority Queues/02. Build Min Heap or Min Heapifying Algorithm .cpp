a7535fa7caba8c60aecc849cdff3e556a8a22bd7
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-08-27 00:04:58 +0530

    Create 02. Build Min Heap or Min Heapifying Algorithm .cpp

---
+++ Module 14 Heaps & Priority Queues/02. Build Min Heap or Min Heapifying Algorithm .cpp
@@ -0,0 +1,44 @@
+// Time Complexity O(log N)
+
+#include <bits/stdc++.h>
+
+void Heapify(vector<int> &arr, int n, int i) {
+
+    int left = (i*2)+1;
+    int right = (i*2)+2;
+    int small = i;
+
+    if(left < n && arr[left] < arr[i]) {
+        swap(arr[i], arr[left]);
+        Heapify(arr,n,left);
+    }
+    if(right < n && arr[right] < arr[i]) {
+        swap(arr[i], arr[right]);
+        Heapify(arr,n,right);
+    }
+
+/*
+    if(left < n && arr[left] < arr[small]) {
+        small = left;
+    }
+
+    if(right < n && arr[right] < arr[small])
+        small = right;
+    
+    if(small != i){
+        swap(arr[small],arr[i]);
+        Heapify(arr,n,small);
+    }
+*/
+}
+
+vector<int> buildMinHeap(vector<int> &arr)
+{
+    int n = arr.size();
+
+    for(int i=n/2-1;i>=0;i--){
+       Heapify(arr,n,i);
+    }
+
+    return arr;
+}
