d746a7dc3525482615e42d3ddd884d339814ee27
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-08-26 23:07:20 +0530

    Update 01. Heap Implementation .cpp

--- Module 14 Heaps & Priority Queues/01. Insertion of Elements in Heap .cpp
+++ Module 14 Heaps & Priority Queues/01. Insertion of Elements in Heap .cpp
@@ -41,6 +41,42 @@ class Heap {
         }
     }
 
+    void DeleteRoot() {
+
+        // Edge Case
+        if(size == 0) {
+            cout<<"Heap is Empty! \n";
+            return;
+        }
+
+        //step 1
+        arr[1] = arr[size];
+
+        //step 2
+        size--;
+
+        //step 3
+        int Pindex = 1;
+        while(Pindex < size){
+
+            int left = Pindex*1;
+            int right = left+1;
+
+            if(left < size && arr[left] > arr[Pindex]) {
+                swap(arr[left] , arr[Pindex]);
+                Pindex = left;
+            }
+            else if(right < size && arr[right] > arr[Pindex]) {
+                swap(arr[right] , arr[Pindex]);
+                Pindex = right;
+            }
+            else{
+                return;
+            }
+        }
+    }
+
+
     void printHeap() {
         for(int i=1; i <= size ; i++) {
             cout<<arr[i]<<" ";
@@ -53,14 +89,29 @@ int main() {
     // cout<<" Hello World \n";
     Heap h1;
     h1.Insert(56);
-    h1.printHeap();
+   // h1.printHeap();
     h1.Insert(65);
     h1.Insert(199);
     h1.Insert(234);
-    h1.printHeap();
+  //  h1.printHeap();
     h1.Insert(45);
     h1.Insert(34);
+    
     h1.printHeap();
+
+    h1.DeleteRoot();
+    
+    /*
+    h1.DeleteRoot();
+    h1.DeleteRoot();
+    h1.DeleteRoot();
+    h1.DeleteRoot();
+    h1.DeleteRoot();
+    h1.DeleteRoot();
+    */
+
+     h1.printHeap();
+
 
     return 0;
 }
