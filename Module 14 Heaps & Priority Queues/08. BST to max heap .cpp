ed5cd9091abea7675fff83b39dfa5eaea63cd5d2
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-09-03 08:35:53 +0530

    Create 08. BST to max heap .cpp

---
+++ Module 14 Heaps & Priority Queues/08. BST to max heap .cpp
@@ -0,0 +1,46 @@
+
+
+
+
+// APPROACH 2
+class Solution{
+  public:
+    
+    void Traversal(Node* root, vector<int>&vect) {
+        
+        if(root == NULL)
+            return;
+        
+        Traversal(root->left, vect);
+        vect.push_back(root->data);
+        Traversal(root->right, vect);
+            
+    }
+    
+    void convertToMaxHeapUtil(Node* root)
+    {
+        vector<int>vect;
+        Traversal(root,vect);
+    //reverse(vect.begin(), vect.end());
+        
+        int size = vect.size();
+        int i=size-1;
+        root->data = vect[i--];
+        root->left = NULL;
+        root->right = NULL;
+        queue<Node*>q;
+        q.push(root);
+        
+        while(i >= 0 ) {          
+            Node* Front = q.front();
+            q.pop();
+            
+            Front->left = newNode(vect[i--]);
+            if(i >= 0)
+                Front->right = newNode(vect[i--]);
+            
+            q.push(Front->left);
+            q.push(Front->right);
+        } 
+    }    
+};
