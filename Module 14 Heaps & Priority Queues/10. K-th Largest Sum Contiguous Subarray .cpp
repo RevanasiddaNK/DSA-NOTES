a1412ca78c8d539bc8e079b706e811666d74566f
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-09-03 15:24:25 +0530

    Update 10. K-th Largest Sum Contiguous Subarray .cpp

--- Module 14 Heaps & Priority Queues/10. K-th Largest Sum Contiguous Subarray .cpp
+++ Module 14 Heaps & Priority Queues/10. K-th Largest Sum Contiguous Subarray .cpp
@@ -2,18 +2,18 @@ class Solution{
 public:
     int kthLargest(vector<int> &Arr,int N,int K){
         priority_queue<int, vector<int>, greater<int> >minHeap;
-	    for(int i=0;i<N; i++){
-	        int sum = 0;
-            for(int j=i; j<N; j++){
-			    sum += Arr[j];
-			    if(minHeap.size() < K)
-			        minHeap.push(sum);
-			    else if(minHeap.top() < sum){
-				    minHeap.pop();
-				    minHeap.push(sum);
-			    }
-            }
+	for(int i=0;i<N; i++){
+		int sum = 0;
+            	for(int j=i; j<N; j++){
+			sum += Arr[j];
+			if(minHeap.size() < K)
+				minHeap.push(sum);
+			else if(minHeap.top() < sum){
+				minHeap.pop();
+				minHeap.push(sum);
+			 }
+            	}
         }
-	    return minHeap.top();
+	return minHeap.top();
     }
 };
