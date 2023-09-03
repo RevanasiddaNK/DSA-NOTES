4ce3a313b2859e2863f551436bc9a0a1be91ade9
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-09-03 10:26:44 +0530

    Update 09. Convert BST to Min Heap .cpp

--- Module 14 Heaps & Priority Queues/09. Convert BST to Min Heap .cpp
+++ Module 14 Heaps & Priority Queues/09. Convert BST to Min Heap .cpp
@@ -1,14 +1,15 @@
+/* 
 Note : Special Heap Property Left < Right
 1. Special Max Heap Post Order Traversal is Sorted in a increasing Order
 2. Special Min Heap Pre Order Traversal is Sorted in a increasing Order
+*/
 
-
-
-
-// APPROACH 1 , find out why this approach is wrong ? this is even wrong for general Min Heap
-// input 8 4 12 2 6 10 14 1 3 5 7 9 11 13 15 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 
-// my  output 1 2 4 8 9 5 10 11 3 6 12 13 7 14 15 
-// Desired Output 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+/* 
+APPROACH 1 , find out why this approach is wrong ? this is even wrong for general Min Heap
+input 8 4 12 2 6 10 14 1 3 5 7 9 11 13 15 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 
+my  output 1 2 4 8 9 5 10 11 3 6 12 13 7 14 15 
+Desired Output 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+*/
  void Traversal(BinaryTreeNode* root, vector<int>&vect) {
         
         if(root == NULL)
