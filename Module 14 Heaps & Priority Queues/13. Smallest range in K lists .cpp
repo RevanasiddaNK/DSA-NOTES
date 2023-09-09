f39020636646ab20b659de9920da47ab8a37a5bf
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-09-09 07:22:22 +0530

    Create 13. Smallest range in K lists .cpp

---
+++ Module 14 Heaps & Priority Queues/13. Smallest range in K lists .cpp
@@ -0,0 +1,84 @@
+/*
+Time Complexity : O(n * k *log k)
+Auxilliary Space  : O(k)
+*/
+
+class Solution{
+    
+    public:
+    
+    class Node{ 
+        
+        public:
+        
+        int data;
+        int row;
+        int col;
+            
+        // constructor
+        Node(int d, int r, int c){
+            data = d;
+            row = r;
+            col = c;
+        }
+    };
+    
+    class compare{
+        public:
+        bool operator() (Node* a, Node* b){
+            return a->data > b->data;
+        }            
+    };
+    
+    pair<int,int> findSmallestRange(int KSortedArray[][N], int n, int k)
+    {
+        
+        pair<int, int>ans;
+        int mini = INT_MAX, maxi = INT_MIN;
+        priority_queue<Node*, vector<Node*>, compare >minHeap;
+        
+        for(int i=0; i<k; i++){
+            
+            Node* temp = new Node(KSortedArray[i][0],i,0);
+            mini = min(mini, KSortedArray[i][0]);
+            maxi = max(maxi, KSortedArray[i][0]);
+            minHeap.push(temp);
+            
+        }
+        
+        int start = mini;
+        int end = maxi;
+        
+        while( !minHeap.empty() ){
+            
+            Node* front = minHeap.top();
+            minHeap.pop();
+            
+            mini = front->data;
+            
+            // Range Updation
+            if(end - start > maxi - mini) {
+                start = mini;
+                end = maxi;
+            }
+            
+            if(front->col+1 < n){
+                
+                maxi = max(maxi, KSortedArray[front->row][front->col+1]);
+                
+                Node* temp = new Node(KSortedArray[front->row][front->col+1], front->row, front->col+1 );
+                minHeap.push(temp);
+                
+            }
+            else
+            {
+                break;
+            }
+             
+        } 
+        
+        ans = make_pair(min(start,end), max(start,end));
+        return ans;
+        
+    }
+};
