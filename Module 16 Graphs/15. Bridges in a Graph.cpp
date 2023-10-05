00961ad6fb1790fed0492ceed1f83c0f6d25df8b
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-10-05 12:39:10 +0530

    Update 15. Bridges in a Graph.cpp

--- Module 16 Graphs/15. Bridges in a Graph.cpp
+++ Module 16 Graphs/15. Bridges in a Graph.cpp
@@ -1,7 +1,12 @@
 /*
 Time Complexity = O(E+V)
 Space Complexity = O(V)
+method : number of bridge elements in circular loop is zero
+         number of bridge elements in stright chain is = number of edges in straight chain
 */
+
+
+
 
 #include<unordered_map>
 #include<list>
@@ -19,6 +24,8 @@ void dfs(int node,int parent,unordered_map<int,list<int>>&adj,vector<int>&Disc,v
                 if(!visited[nbr]){
                     dfs(nbr,node,adj,Disc,Low,visited,ans,timer);
                     Low[node] = min(Low[node],Low[nbr]);
+
+                        // Bridge Edge Condition if they are in stright chains
                     if(Low[nbr] > Disc[node]){
                        vector<int>temp;
                        temp.push_back(node);
@@ -28,6 +35,10 @@ void dfs(int node,int parent,unordered_map<int,list<int>>&adj,vector<int>&Disc,v
                 }
                 else{
                     // Back Edge Case
+                // it means it is forming circular loop inside a graph
+                // all the low[ all nodes in this circular loop ] = min(Low[node],Disc[nbr]);
+                // due to this always low[nbr] == low[node] hence
+                // all the edges in the circular loop are aren't to inserted int ans; bcz we have condition  if(Low[nbr] > Disc[node]) 
                     Low[node] = min(Low[node],Disc[nbr]);
                 }
 
@@ -55,6 +66,7 @@ vector<vector<int>> findBridges(vector<vector<int>> &edges, int v, int e) {
 
     int timer = 0;
     int parent = -1;
+// DFS Algorithm of DisConnected Graphs
     for(int i=0; i<v; i++){
         if(!visited[i]){
             dfs(i,parent,adj,Disc,Low,visited,ans,timer);
