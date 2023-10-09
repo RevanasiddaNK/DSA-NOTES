8e3bd4c34b956959725fc02a3aade3188966e601
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-10-09 16:00:57 +0530

    Update 05. Max Sum without Adjacents.cpp

--- Module 17 Dynamic Programming/05. Max Sum without Adjacents.cpp
+++ Module 17 Dynamic Programming/05. Max Sum without Adjacents.cpp
@@ -1,59 +1,76 @@
-// Recursive Solution 
-	int solveRec(int *nums,int n, int size){
-        if(n >= size)
-            return 0;
-    
-        int incl = solveRec(nums,n+2,size)+nums[n];
-        int excl = solveRec(nums,n+1,size)+0;
+#include <bits/stdc++.h> 
 
-        return max(incl,excl);
-    }
-	
-	
-	// Memoization + Recursion 
-	int solveMem1(int *nums,int n, int size,int dp[]){
-        if(n >= size)
-            return 0;
-        
-        if(dp[n] != -1)
-            return dp[n];
+// Recursion
+int solveRec(vector<int> &nums, int n){
+    if(n < 0)
+        return 0;
     
-        int incl = solveMem1(nums,n+2,size,dp)+nums[n];
-        int excl = solveMem1(nums,n+1,size,dp)+0;
+    int pick = solveRec(nums,n-2)+nums[n];
+    int notpick = solveRec(nums,n-1)+0;
 
-        dp[n] = max(incl,excl);
+    return max(pick,notpick);
+}
+
+// Memoization
+int solveMem(vector<int> &nums, int n,vector<int>&dp){
+    if(n < 0)
+        return 0;
+
+    if(dp[n] != -1)
         return dp[n];
+    
+    int pick = solveMem(nums,n-2,dp)+nums[n];
+    int notpick = solveMem(nums,n-1,dp)+0;
+
+    return dp[n] = max(pick,notpick);
+}
+
+// Tabulation
+int solveTab(vector<int> &nums){
+    int n = nums.size();
+    vector<int>dp(n+1,-1); 
+    dp[0] = nums[0];
+    for(int i=1; i<n; i++){
+
+        int pick = nums[i];
+        // Edge Case
+        if(i != 1)
+            pick += dp[i-2];
+
+        int notpick = 0+dp[i-1];
+
+        dp[i] = max(pick,notpick);
     }
-    
-    int solveMem2(int *nums,int n, int size,int dp[]){
-        if(n < 0)
-            return 0;
-        
-        if(dp[n] != -1)
-            return dp[n];
-    
-        int incl = solveMem2(nums,n-2,size,dp)+nums[n];
-        int excl = solveMem2(nums,n-1,size,dp)+0;
+    return dp[n-1];
+}
 
-        dp[n] = max(incl,excl);
-        return dp[n];
+// Space Optimized Approach
+int solveSpaceOpt(vector<int> &nums){
+    int n = nums.size();
+    int prev2 = 0;          // nums[-1] case negative index
+    int prev1 = nums[0];    // dp[0] = nums[0];
+    for(int i=1; i<n; i++){
+        int pick = nums[i]+prev2;
+        int notpick = 0+prev1;
+        // dp[i] = max(pick,notpick); curr
+        int curr = max(pick, notpick);
+        prev2 = prev1;
+        prev1 = curr;
     }
-   
-	int findMaxSum(int *arr, int n) {
-	    
-	    int ans = 0;
-	    //Recursion 
-//	    ans = solveRec(arr,0,n);
-	    
-	    // Memoization 
-	    int dp[n+1];
-	    for(int i=0; i<=n; i++)
-	        dp[i] = -1;
-	    
-	    ans = solveMem1(arr,0,n,dp);
-        //ans = solveMem2(arr,n-1,n,dp);
+    return prev1;
+}
 
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int ans;
+    int n = nums.size();
+//    ans = solveRec(nums,n-1);
+
+    vector<int>dp(n+1,-1);
     
-        return ans;
-
-	}
+//    ans = solveMem(nums,n-1,dp);
+//    ans = solveTab(nums);
+    ans = solveSpaceOpt(nums);
+ 
+    return ans;
+}
