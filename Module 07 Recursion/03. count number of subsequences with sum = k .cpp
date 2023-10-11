a61132d9e7f02c8a96d3061eabcfa74777cf850f
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-10-11 13:31:23 +0530

    Update 03. count number of subsequences with sum = k .cpp

--- Module 07 Recursion/03. count number of subsequences with sum = k .cpp
+++ Module 07 Recursion/03. count number of subsequences with sum = k .cpp
@@ -2,30 +2,6 @@
 #include<vector>
 using namespace std;
 
-void print(vector<int>&temp){
-    for(int i=0; i<temp.size(); i++){
-        cout<<temp[i]<<" ";
-    }
-    cout<<endl;
-}
-void solveRec(int arr[], vector<int>&temp, int index, int n, int &sum, int k){
-    if(index >= n){
-        if(sum == k){
-            print(temp);
-        }
-        return;
-    }
-    // pick 
-    temp.push_back(arr[index]);
-    sum += arr[index];
-    solveRec(arr,temp,index+1,n,sum,k);
-
-    // not pick
-    temp.pop_back();
-    sum -= arr[index];
-    solveRec(arr,temp,index+1,n,sum,k);
-}
-
 int countnumberofsubsequeces(int arr[], int index, int n, int &sum, int k){
 
     if(index >= n){
@@ -55,8 +31,7 @@ int main(){
     int index = 0;
     int sum = 0;
     int n = 10;
-    // solveRec(arr,temp,index,n,sum,k);
-
+   
     cout<<"number of subsequeces with sum = "<<k<<" is "<<countnumberofsubsequeces(arr,index,n,sum,k)<<endl;
 
     return 0;
