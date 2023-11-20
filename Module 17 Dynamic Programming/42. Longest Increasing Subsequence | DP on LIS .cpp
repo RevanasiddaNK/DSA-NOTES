b32717cbd25a1175074b128e1f5ab8632c666a50
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-11-20 19:58:16 +0530

    Update 42. Longest Increasing Subsequence | DP on LIS .cpp

--- Module 17 Dynamic Programming/42. Longest Increasing Subsequence | DP on LIS .cpp
+++ Module 17 Dynamic Programming/42. Longest Increasing Subsequence | DP on LIS .cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+
 int solveRec(int ind, int prevInd, int arr[], int n){
     if(ind >= n)
         return 0;
@@ -27,15 +28,60 @@ int solveMem(int ind, int prevInd, int arr[], int n,vector<vector<int>>&dp){
     return dp[ind][prevInd] = max(take,notTake);
 }
 
+int solveTab(int arr[], int n){
+    vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
+    //Base Case
+    for(int prevInd=n+1; prevInd >=0; prevInd--)
+        dp[n+1][prevInd] = 0;
+    
+    // changing parameters nested for loops 
+    for(int ind = n; ind>=1; ind--){
+        for(int prevInd= ind-1; prevInd >= 0; prevInd--){
+            int notTake = 0 + dp[ind+1][prevInd];
+            int take = 0;
+            if(prevInd == 0 || arr[prevInd-1] < arr[ind-1] )
+                take = 1 + dp[ind+1][ind];
+
+            dp[ind][prevInd] = max(take,notTake);
+        }
+    }
+    return dp[1][0];
+}
+
+int solveSpaceOpt(int arr[], int n){
+    vector<int>prev(n+1,-1);
+    //Base Case
+    for(int prevInd=n; prevInd >=0; prevInd--)
+        prev[prevInd] = 0;
+    
+    // changing parameters nested for loops 
+    for(int ind = n; ind>=1; ind--){
+        vector<int>curr(n+1,-1);
+        for(int prevInd= ind-1; prevInd >= 0; prevInd--){
+            int notTake = 0 + prev[prevInd];
+            int take = 0;
+            if(prevInd == 0 || arr[prevInd-1] < arr[ind-1] )
+                take = 1 + prev[ind];
+
+            curr[prevInd] = max(take,notTake);
+        }
+        prev = curr;
+    }
+    return prev[0];
+}
 
 int longestIncreasingSubsequence(int arr[], int n)
 {
     int prevInd = -1;
     int ind = 0;
-    //return solveRec(ind,prevInd,arr,n);
+    return solveRec(ind,prevInd,arr,n);
 
     prevInd = 0;
     ind = 1;
     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
     return solveMem(ind,prevInd,arr,n,dp);
+
+    return solveTab(arr,n);
+
+    return solveSpaceOpt(arr,n);
 }
