1986972662fc463aecc0ca8cdd40fd75aea61ebe
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-11-25 19:54:48 +0530

    Create 47. Longest Bitonic Sequence | DP on LIS .cpp

---
+++ Module 17 Dynamic Programming/47. Longest Bitonic Sequence | DP on LIS .cpp
@@ -0,0 +1,23 @@
+int longestBitonicSubsequence(vector<int>& arr, int n)
+{
+    vector<int>dp1(n,1);
+	for(int i=1; i<n; i++){
+        for(int prev=0; prev<i; prev++){
+            if(arr[i] > arr[prev] && dp1[i] < 1 + dp1[prev]){
+                dp1[i] = 1 + dp1[prev];
+            }
+        }
+    }
+
+    int maxi = 1;
+    vector<int>dp2(n,1);
+	for(int i=n-1; i >= 0; i--){
+        for(int prev=n-1; prev > i; prev--){
+            if( arr[i] > arr[prev]  && dp2[i] < 1 + dp2[prev]){
+                dp2[i] = 1 + dp2[prev];
+            }
+        }
+        maxi = max(maxi,dp1[i]+dp2[i]-1);
+    }
+    return maxi;
+}
