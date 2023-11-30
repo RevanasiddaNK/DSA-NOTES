2ce5bb57a9e02b0d36e888e779f0cf7ac42032fb
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-11-30 18:56:25 +0530

    Create 06. Minimum Cost of ropes .cpp

---
+++ Module 18 Greedy Algorithms/06. Minimum Cost of ropes .cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    
+    long long minCost(long long arr[], long long n) {
+        
+        priority_queue<long long,vector<long long>,greater<long long>>pq;
+        for(long long i=0; i<n; i++)
+            pq.push(arr[i]);
+        
+        long long cost = 0;    
+        while(pq.size() > 1){
+            
+            long long minLen1 = pq.top();
+            pq.pop();
+            long long minLen2 = pq.top();
+            pq.pop();
+            
+            long long ToConnect = minLen1 + minLen2;
+            cost += ToConnect;
+            pq.push(ToConnect);
+            
+        }
+        return cost;
+    }
+};
