82b925dfe328ae8e83154e727b7ac6f797faf9d5
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2023-11-30 15:51:49 +0530

    Update Largest Rectangle in Histogram .cpp

--- Module 11.A Stacks/Largest Rectangle in Histogram .cpp
+++ Module 11.A Stacks/Largest Rectangle in Histogram .cpp
@@ -43,3 +43,31 @@ int largestRectangle(vector<int>&heights) {
   }
   return maxArea;
 }
+
+
+
+
+
+
+#include<bits/stdc++.h>
+int largestRectangle(vector<int>&heights) 
+{
+  int n = heights.size();
+   stack<int>st;
+  int maxArea = 0;
+  for(int i=0; i<=n; i++){
+    while(!st.empty() && (i == n || heights[st.top()] >= heights[i])){
+      int hight = heights[st.top()];
+      st.pop();
+      int width = 0;
+
+      if(st.empty())
+        width = i;
+      else
+        width = i - st.top() - 1;
+        maxArea = max(maxArea,width*hight);
+    }
+    st.push(i);
+  }
+  return maxArea;
+}
